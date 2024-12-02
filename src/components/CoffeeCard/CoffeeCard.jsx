f115f4bedd38285ff18d85a17cbe3bf44efeb265
hasibprangon <hasibulhossainp09@gmail.com>
2024-12-02 19:54:14 +0600

    deleted a coffee

--- src/components/CoffeeCard/CoffeeCard.jsx
+++ src/components/CoffeeCard/CoffeeCard.jsx
@@ -2,9 +2,43 @@ import React from 'react';
 import { FaEye } from "react-icons/fa";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+    const handleDelete = (_id) => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                console.log('Delete Confirmed');
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method:'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deleteCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+
+            }
+        });
+    }
     return (
         <div>
             <div className="card card-side  bg-[#f1ead4] shadow-xl">
@@ -24,7 +58,9 @@ const CoffeeCard = ({ coffee }) => {
                         <div className="join join-vertical gap-2">
                             <button className="rounded-lg p-3 bg-[#D2B48C] flex justify-center items-center join-item"><FaEye /></button>
                             <button className="rounded-lg p-3 bg-[#3C393B] flex justify-center items-center join-item text-white"><FaRegEdit /></button>
-                            <button className="rounded-lg p-3 bg-[#EA4744] flex justify-center items-center join-item text-lg"><MdDelete /></button>
+                            <button
+                                onClick={() => handleDelete(_id)}
+                                className="rounded-lg p-3 bg-[#EA4744] flex justify-center items-center join-item text-lg"><MdDelete /></button>
                         </div>
                     </div>
                 </div>
